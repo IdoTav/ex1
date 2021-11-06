f579ee761f5a6bf9ef1a0b68d5e397d2ba5efef6
Tal Dabran <dabrantal@gmail.com>
2021-11-06 20:21:51 +0200

    final ex2

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -6,14 +6,31 @@
 #include <vector>
 #define THERSHOLD 0.9
 
+SimpleAnomalyDetector::SimpleAnomalyDetector() {
+}
 
-float getMaxDev(Point** pointArr, Line line, int size) {
+SimpleAnomalyDetector::~SimpleAnomalyDetector() {
+}
+
+/**
+ * this function get ptrPointArr and line, she is returns the most long distance between the
+ * given line to the farest point
+ * @param ptrPointArr is a pointer to array with pointers who points to Point objects
+ * @param line is a given line
+ * @param size is the pointers array size
+ * @return the farest point from the line
+ */
+float getMaxDev(Point** ptrPointArr, Line line, int size) {
+    //case the line was created by default constructor, so we can do nothing with him.
     if (0 == line.a && 0 == line.b)
         return -1;
     else {
+        //initialize variable who hold the maximum value
         float maxDev = 0;
+        //run all over the Point objects from the pointers array
         for (int i = 0; i < size; i++) {
-            float tmpDev = dev(*pointArr[i], line);
+            //call the function who calculate the distance between given point to the line
+            float tmpDev = dev(*ptrPointArr[i], line);
             if (tmpDev >= maxDev)
                 maxDev = tmpDev;
         }
@@ -21,42 +38,56 @@ float getMaxDev(Point** pointArr, Line line, int size) {
     }
 }
 
+
+/**
+ * this function takes floats vector and convert him to the array
+ * @param vec is vector who will be copied to an array
+ * @param array is a pointer to float array who will contain the floats from the vector
+ */
 void fromVectorToFloatArray(vector<float>vec, float array[]) {
     for(int i = 0; i<vec.size(); i++) {
         array[i] = vec[i];
     }
 }
 
-void initPointsArray (int size, Point** array, Point p[]) {
+
+/**
+ * this function takes pointer to ptrArray with pointers who points to Point objects and inserts the Point objects
+ * to simple Points ptrArray
+ * @param size is the pointers array size
+ * @param ptrArray pointer to array with pointers who points to Point objects
+ * @param p is a pointer to Point objects array who will contain the points from ptrArray
+ */
+void initPointsArray (int size, Point** ptrArray, Point p[]) {
     for (int i =0; i < size; i++) {
-        array[i] = &p[i];
+        ptrArray[i] = &p[i];
     }
 }
 
-SimpleAnomalyDetector::SimpleAnomalyDetector() {
-}
-
-SimpleAnomalyDetector::~SimpleAnomalyDetector() {
-}
-
-
+/**
+ * this function gets data and run a normal learn model to find the best correlations between any features in given
+ * time series
+ * @param ts hold the data from given database in map
+ */
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
     vector<string>keysVector = ts.getKeysVector();
-    vector<string>::iterator it;
-    vector<string>::iterator it2;
-    for (it = keysVector.begin(); it != keysVector.end(); it++) {
-        int arraySize = (int)ts.getValueByKey(*it).size();
-        float array1[ts.getValueByKey(*it).size()];
-        fromVectorToFloatArray(ts.getValueByKey(*it), array1);
+    //run the main keys (feature 1)
+    for (auto it = keysVector.begin(); it != keysVector.end(); it++) {
+        int arraySize = (int) ts.getValuesByKey(*it).size();
+        float array1[ts.getValuesByKey(*it).size()];
+        fromVectorToFloatArray(ts.getValuesByKey(*it), array1);
         correlatedFeatures tmp;
         tmp.feature1 = *it;
+        //set the minimum threshold we can live with
         float bestCor = THERSHOLD;
         Point* pointArr[arraySize];
         Point p[arraySize];
-        for(it2 = it + 1; it2 != keysVector.end(); it2++){
-            float array2[ts.getValueByKey(*it2).size()];
-            fromVectorToFloatArray(ts.getValueByKey(*it2), array2);
+        // run the second key (feature 2) by initializing the iterator to the next to the main key
+        for(auto it2 = it + 1; it2 != keysVector.end(); it2++){
+            float array2[ts.getValuesByKey(*it2).size()];
+            fromVectorToFloatArray(ts.getValuesByKey(*it2), array2);
             float curPearson = absolute(pearson(array1, array2, arraySize));
+            //if we find a batter correlation between the main key to another key set him into the correlated feature
             if (curPearson >= bestCor) {
                 tmp.feature2 = *it2;
                 tmp.corrlation = curPearson;
@@ -69,27 +100,38 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
             }
         }
         float maxDev = getMaxDev(pointArr, tmp.lin_reg, arraySize);
+        //case there is no maxDev
         if(-1 == maxDev)
             continue;
+        //make sure the threshold will be accurate
         else
-            tmp.threshold = 1.1 * maxDev;
+            tmp.threshold = (float) 1.1 * maxDev;
+        //add the correlated feature to the correlated features vector
         cf.push_back(tmp);
     }
 }
 
-
+/**
+ * this function gets data and looking for anomalies between the data to the normal model who has been learned
+ * before
+ * @param ts hold the data from given database in map
+ * @return vector with anomaly reports
+ */
 vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts) {
-    vector<correlatedFeatures>::iterator it;
     vector<AnomalyReport> arVector;
-    for(int i = 0; i < ts.getValueByKey(ts.getKeysVector()[1]).size(); i++) {
+    //run all over the rows on the given data
+    for(int i = 0; i < ts.getValuesByKey(ts.getKeysVector()[1]).size(); i++) {
         int j = 0;
-        for (it = cf.begin(); it != cf.end(); it++) {
+        //looking for correlated features who can be compared with specific points in a row
+        for (auto it = cf.begin(); it != cf.end(); it++) {
             float xVal = ts.getValByKeyAndIndex(cf[j].feature1, i), yVal = ts.getValByKeyAndIndex(cf[j].feature2, i);
             float curDev = cf[j].threshold;
             Point p(xVal, yVal);
+            //check if there is an anomaly
             if (curDev < dev(p, cf[j].lin_reg)) {
                 string desc = cf[j].feature1 + "-" + cf[j].feature2;
                 AnomalyReport ar(desc, i + 1);
+                //add the anomaly to the anomaly reports
                 arVector.push_back(ar);
             }
             j++;
--- timeseries.cpp
+++ timeseries.cpp
@@ -1,20 +1,26 @@
 #include "timeseries.h"
 #include "SimpleAnomalyDetector.h"
 
-vector<float> getValueByKey(const string &basicString);
-
+/**
+ * this function get valueVector and insert the values under appropriate key
+ * @param valueVector is a values who need to been inserted to the map
+ * @param initMap is the map who will contain the data
+ */
 void TimeSeries::initializeMap(vector<float> valueVector, map<string, vector<float>> *initMap) {
     vector<float> vectorArray[_keysVector.size()];
     for (int i = 0; i < valueVector.size(); i++)
         vectorArray[i % _keysVector.size()].push_back(valueVector[i]);
-    vector<string>::iterator it;
     int i = 0;
-    for (it = _keysVector.begin(); it != _keysVector.end(); it++) {
+    for (auto it = _keysVector.begin(); it != _keysVector.end(); it++) {
         initMap->insert({_keysVector[i], vectorArray[i]});
         i++;
     }
 }
 
+/**
+ * this function split string by comma and insert the strings who was seperated to keys vector
+ * @param currentLine  string who need to been split by commas
+ */
 void TimeSeries::getKeysFromString(const string& currentLine) {
     stringstream strArr(currentLine);
     while (strArr.good()) {
@@ -24,6 +30,11 @@ void TimeSeries::getKeysFromString(const string& currentLine) {
     }
 }
 
+/**
+ * this function split string by comma and insert the strings who was seperated to values vector
+ * @param currentLine  string who need to been split by commas
+ * @param valueVector vector who will hold the values
+ */
 void TimeSeries::getValueFromString(const string& currentLine, vector<float> *valueVector) {
     stringstream strArr(currentLine);
     while (strArr.good()) {
@@ -33,21 +44,39 @@ void TimeSeries::getValueFromString(const string& currentLine, vector<float> *va
     }
 }
 
+/**
+ * getter to the keys Vector
+ * @return the keys Vector
+ */
 vector<string> TimeSeries:: getKeysVector()const {
     return _keysVector;
 }
 
-vector<float> TimeSeries:: getValueByKey(const string& key)const {
+/**
+* getter for values from the map who has been detected by key
+ * @param key help us to detect the values vector in the map
+ * @return the specific values who has been detected
+ */
+vector<float> TimeSeries:: getValuesByKey(const string& key)const {
     auto it = _mapData.find(key);
     return it->second;
 }
 
+/**
+ * getter for value from the map who has been detected by key and index
+ * @param key help us to detect the values vector in the map
+ * @param i help us to detect the value in the vector
+ * @return the specific value who has been detected
+ */
 float TimeSeries::getValByKeyAndIndex (const string& key, int i)const {
-    vector<float> values = getValueByKey(key);
+    vector<float> values = getValuesByKey(key);
     return values[i];
 }
 
-
+/**
+ * set the data base and insert the data to a map
+ * @param CSVfileName is CSV file name who will be our data
+ */
 TimeSeries::TimeSeries(const char *CSVfileName) {
     ifstream myFile;
     myFile.open(CSVfileName);
