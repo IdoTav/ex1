f8c5b1e53fe12e61a7c117282c1b46574c78adda
Tal Dabran <dabrantal@gmail.com>
2021-12-20 12:42:12 +0200

    SWITCH CASE MENU

--- CLI.cpp
+++ CLI.cpp
@@ -3,7 +3,44 @@
 CLI::CLI(DefaultIO* dio) {
 }
 
+void printMenu() {
+    std::cout<<"Welcome to the Anomaly Detection Server.\n"
+               "Please choose an option:\n"
+               "1. upload a time series csv file\n"
+               "2. algorithm settings\n"
+               "3. detect anomalies\n"
+               "4. display results\n"
+               "5. upload anomalies and analyze results\n"
+               "6. exit"<<std::endl;
+}
+
 void CLI::start(){
+    printMenu();
+    int i = 0;
+    while(true) {
+        std::cin >> i;
+        if (6 == i)
+            break;
+        switch (i) {
+            case(1): {
+                //UPLOAD A TIME SERIES CSV FILE
+            }
+
+            case(2): {
+                //ALGORITHM SETTINGS
+            }
+            case(3): {
+                //DETECT ANOMALIES
+            }
+            case(4): {
+                //DISPLAY RESULTS
+            }
+            case(4): {
+                //UPLOAD ANOMALIES AND ANALYZE RESULTS
+            }
+        }
+    }
+
 }
 
 
